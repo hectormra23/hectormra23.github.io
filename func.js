11ac08dd48d0750133c052758521f1eee27c8415
Hectormra23 <88894715+hectormra23@users.noreply.github.com>
2022-08-07 21:38:55 -0500

    Add files via upload

---
+++ func.js
@@ -0,0 +1,13 @@
+console.log("funciona UwU")
+/*scrol de raton */
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.news-cards', { delay: 500 });
+  ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
+
+  
